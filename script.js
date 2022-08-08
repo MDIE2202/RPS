10987d28b8d1ee17e42ad2485dc8531b307fc721
MDIE2202 <marty.diender@gmail.com>
2022-08-08 13:33:40 +0200

    Added a function for deciding the winning team. This is based on a set of if statements.

--- script.js
+++ script.js
@@ -14,7 +14,43 @@ function getComputerChoice() {
     }
 
     return choice;
+}
 
+playerSelection = (player, computer) => {
+//determines the outcome: 1: player wins 0: computer wins 2:draw 3: no valid input
+    let outcome = '';
 
+    if (computer == 'rock' && player == 'paper'){
+        outome = 'Player wins!' + player + ' beats ' + computer + ' !';
+    }
+    else if (computer == 'scissors' && player == 'paper'){
+        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+    }
+    else if (computer == 'paper' && player == 'paper'){
+        outcome = 'Draw! You both picked ' + computer
+    }
+    else if (computer == 'rock' && player == 'rock'){
+        outcome = 'Draw! You both picked ' + computer
+    }
+    else if (computer == 'scissors' && player == 'rock'){
+        outcome = 'Player wins!' + player + ' beats ' + computer + ' !';
+    }
+    else if (computer == 'paper' && player == 'rock'){
+        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+    }
+    else if (computer == 'rock' && player == 'scissors'){
+        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+    }
+    else if (computer == 'scissors' && player == 'scissors'){
+        outcome = 'Draw! You both picked ' + computer;
+    }
+    else if (computer == 'paper' && player == 'scissors'){
+        outcome = 'Player wins!' + player + ' beats ' + computer + ' !';
+    }
+    else{
+        outcome = 'Invalid input, please put "rock", "paper" or "scissors"' + outcome;
+    }
+
+    return outcome;
 }
 
