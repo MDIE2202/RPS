af04123408e8d0b251a2bc03e64ebf7e479939fe
MDIE2202 <marty.diender@gmail.com>
2022-08-08 13:53:04 +0200

    Added a loop to play 5 rounds of the game declaring a winner afterwards

--- script.js
+++ script.js
@@ -18,39 +18,75 @@ function getComputerChoice() {
 
 playerSelection = (player, computer) => {
 //determines the outcome: 1: player wins 0: computer wins 2:draw 3: no valid input
-    let outcome = '';
+    let outcome = 3;
 
     if (computer == 'rock' && player == 'paper'){
-        outome = 'Player wins!' + player + ' beats ' + computer + ' !';
+        outcome = 1;
     }
     else if (computer == 'scissors' && player == 'paper'){
-        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+        outcome = 0;
     }
     else if (computer == 'paper' && player == 'paper'){
-        outcome = 'Draw! You both picked ' + computer
+        outcome = 2;
     }
     else if (computer == 'rock' && player == 'rock'){
-        outcome = 'Draw! You both picked ' + computer
+        outcome = 2;
     }
     else if (computer == 'scissors' && player == 'rock'){
-        outcome = 'Player wins!' + player + ' beats ' + computer + ' !';
+        outcome = 1;
     }
     else if (computer == 'paper' && player == 'rock'){
-        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+        outcome = 0;
     }
     else if (computer == 'rock' && player == 'scissors'){
-        outcome = 'Computer wins!' + computer + ' beats ' + player + ' !'
+        outcome = 0;
     }
     else if (computer == 'scissors' && player == 'scissors'){
-        outcome = 'Draw! You both picked ' + computer;
+        outcome = 2;
     }
     else if (computer == 'paper' && player == 'scissors'){
-        outcome = 'Player wins!' + player + ' beats ' + computer + ' !';
+        outcome = 1;
     }
     else{
-        outcome = 'Invalid input, please put "rock", "paper" or "scissors"' + outcome;
+        outcome = 3;
     }
 
     return outcome;
 }
+
+game = () => {
+    let player_score = 0;
+    let computer_score = 0;
+
+    for (let i = 0; i < 5; i++){
+        result = playerSelection(prompt(), getComputerChoice());
+        if (result == 0){
+            computer_score += 1;
+            console.log('computer wins')
+        }
+        else if (result == 1){
+            player_score += 1;
+            console.log('you win')
+        }
+        else if (result == 2){
+            player_score += 1;
+            computer_score += 1;
+            console.log('DRAW')
+        }
+        else{
+            return 'no valid input has been given'
+        }
+    }
+
+    if (player_score > computer_score){
+        return 'You win with '+ player_score + ' VS ' + computer_score + '!' 
+    }
+    else if (player_score < computer_score){
+        return 'You lose with '+ player_score + ' VS ' + computer_score + '!' 
+    }
+    else{
+        return 'It s a DRAW!'
+    }
+}
+
 
